35665761e11116f48a7a86597522c6079e2d5814
encoderit-touhid <touhid@encoderit.net>
2026-09-04 15:11:54 +0600

    Update OrderSerializer with order_name and user_string fields

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,9 @@
 from dataclasses import fields
+from itertools import product
 from pyexpat import model
 
 from rest_framework import serializers
-from .models import Product, Order, OrderItem
+from .models import Product, Order, OrderItem,User
 
 class ProductSerializer(serializers.ModelSerializer):
       class Meta:
@@ -15,13 +16,30 @@ class ProductSerializer(serializers.ModelSerializer):
            return value
 class ItemSerializer(serializers.ModelSerializer):
     product = ProductSerializer(read_only=True)
+    product_name=serializers.CharField(source='product.name')
+    product_price = serializers.DecimalField(max_digits=10,decimal_places=2,source='product.price')
     class Meta:
         model = OrderItem
-        fields = ['quantity', 'item_subtotal','product']
-        
+        fields = ['quantity', 'item_subtotal','product','product_name','product_price']
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'first_name','last_name']
+                
 class OrderSerializer(serializers.ModelSerializer):
     items = ItemSerializer(many=True, read_only=True)
+    user =  UserSerializer(read_only=True)
+    total_orders=serializers.SerializerMethodField(method_name="total_order")
+    order_name=serializers.CharField(source='__str__', read_only=True)
+    user_string=serializers.StringRelatedField(source='user')
+    # total_orders=serializers.SerializerMethodField()
     
+    # def get_total_orders(self,object):
+    #     order_items = object.items.all()
+    #     return sum(single_item.item_subtotal for single_item in order_items)
+    def total_order(self,object):
+            order_items = object.items.all()
+            return sum(single_item.item_subtotal for single_item in order_items) 
     class Meta:
             model  = Order
-            fields = ('order_id','user','status','items')       
+            fields = ('order_id','order_name','user','status','items','total_orders','user_string')       
